895a49b18f921d5bb522a7a317f0071c6c3b9551
itsmeakhil <akhiledathadan007@gmail.com>
2022-06-19 12:50:15 +0530

    updated start script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 echo "Starting Picman API Django application"
 export DJANGO_SETTINGS_MODULE="picman_studio_backend.settings"
-gunicorn --workers=3 --bind=0.0.0.0:8000 mind_map.wsgi:application  --threads=3 --worker-connections=500 --log-level=debug --timeout=120
+gunicorn --workers=3 --bind=0.0.0.0:8000 picman_studio_backend.wsgi:application  --threads=3 --worker-connections=500 --log-level=debug --timeout=120
